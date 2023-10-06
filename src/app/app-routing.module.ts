b85be73fbe41e9c6318640f75f1d3f0ddcfe0354
Jordan Haugen <jordan.t.haugen@gmail.com>
2023-10-06 10:50:56 -0400

    adding proper routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorldComponent } from './world/world.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: WorldComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
